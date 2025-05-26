2eb0c80765ef00001be45aa0b23769d447de2a9d
Ionescu Emanuel <emanuel.ionescu27@gmail.com>
2025-05-26 13:47:36 +0300

    Revert "Image Transmision Update"
    This reverts commit bde74443159de09544382338191c1f13d780829f.

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -1,3 +1,8 @@
+DEBUG = True
+def ECHO(arg):
+    if DEBUG:
+        print(arg)
+
 import json
 with open('./resources/data/config.json', 'r') as file:
     hardware_data = json.load(file)
@@ -6,15 +11,12 @@ IMAGE_PROCESSORS = [
     hardware_data["Platform"]["ImageProcessing1"],
     hardware_data["Platform"]["ImageProcessing2"]
     ]
+
 FEEDBACKS = [
     hardware_data["Platform"]["GUIFeedback1"],
     hardware_data["Platform"]["GUIFeedback2"]
 ]
-CAMERAS = list(hardware_data["Camera"].values())
-TAPO_CAM_AUTH = {
-    "User" : "TapoCam",
-    "Password" : "salut123"
-}
+
 CAMERA_RESOLUTION = (1280, 720)
 FRAME_SLICE_INFO = [
     [int(CAMERA_RESOLUTION[0]/4), int(3 * CAMERA_RESOLUTION[0]/4), int(CAMERA_RESOLUTION[1]/4), int(3 * CAMERA_RESOLUTION[1]/4), "w:0.25:0.5,h:0.25:0.5"], 
@@ -31,7 +33,6 @@ import threading
 import time
 import argparse
 import os
-import cv2
 
 parser = argparse.ArgumentParser()
 parser.add_argument("ID", help="Image Processor ID", type=int)
@@ -49,6 +50,7 @@ def loading_bar(est):
 
 
 def main():
+
     loading_time = time.time()
     estimated_loading_time = 2
 
@@ -68,22 +70,7 @@ def main():
     t1.join()
     print("ML loaded in", loading_time, "seconds")
 
-    pipeline = 'imxcompositor_g2d name=comp sink_0::xpos=0 sink_0::ypos=0 sink_1::xpos=0 sink_1::ypos=720 ' \
-                '! queue ! appsink sync=false rtspsrc location="rtsp://{cam1_u}:{cam1_p}@{cam1_ip}/stream2" ! rtph264depay ! h264parse ! queue ! v4l2h264dec ' \
-                '! queue ! comp. rtspsrc location="rtsp://{cam2_u}:{cam2_p}@{cam2_ip}/stream2" ! rtph264depay ! h264parse ! queue ! v4l2h264dec ! queue ! comp.'.format(
-                cam1_u  = TAPO_CAM_AUTH["User"],
-                cam1_p  = TAPO_CAM_AUTH["Password"],
-                cam1_ip = CAMERAS[0]["IP"],
-                cam2_u  = TAPO_CAM_AUTH["User"],
-                cam2_p  = TAPO_CAM_AUTH["Password"],
-                cam2_ip = CAMERAS[1]["IP"] 
-            )
-    
-    cam = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
-    
-    ok, raw_frame = cam.read()
-    frame = raw_frame[(ID - 1) * 720 : ID * 720, :, :]  
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+    message, frame = IR.receive()
 
     faces = AI.find_faces(frame)
     body = AI.find_body(frame)
@@ -93,44 +80,49 @@ def main():
 
     print("Starting...")
     while True:
+        
         os.system("clear")
         print("                         \rFPS:", 1/(time.time() - fps_time), end='\r')
         fps_time =  time.time()
+
         to_be_sent = {}
-        ok, raw_frame = cam.read()
-        frame = raw_frame[(ID - 1) * 720 : ID * 720, :, :]  
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-        option = "all"
 
-        # if(option == "add"):
-        #     username = width # working on older code where the second argument was width
-        #     faces = AI.find_faces(frame)
-        #     face = faces[0][0]
-        #     face *= 300
-        #     G = [(face[0]+face[2]) * .5, (face[1]+face[3]) * .5]
-        #     to_be_sent["G"].append(str(G[0]/300) + ", " + str(G[1]/300))
-        #     dist = min(face[2] - face[0], face[3] - face[1]) * .5
+        message, frame = IR.receive()
+        message = message.split()[0]
+        option, width, height = message.split(',')
 
-        #     face[0] = G[0] - dist
-        #     face[1] = G[1] - dist
-        #     face[2] = G[0] + dist
-        #     face[3] = G[1] + dist
+        print("\nInstruction:", message)
+        print("Face: ", end="")
 
-        #     face = np.array(face, np.int32)
+        if(option == "add"):
+            username = width # working on older code where the second argument was width
+            faces = AI.find_faces(frame)
+            face = faces[0][0]
+            face *= 300
+            G = [(face[0]+face[2]) * .5, (face[1]+face[3]) * .5]
+            to_be_sent["G"].append(str(G[0]/300) + ", " + str(G[1]/300))
+            dist = min(face[2] - face[0], face[3] - face[1]) * .5
 
-        #     cropped_frame = frame[face[0] : face[2], face[1] : face[3]]
-        #     mask = AI.id_face(cropped_frame)
-        #     with open('./resources/data/users.json', 'r') as file:
-        #         users_data = json.load(file)
+            face[0] = G[0] - dist
+            face[1] = G[1] - dist
+            face[2] = G[0] + dist
+            face[3] = G[1] + dist
+
+            face = np.array(face, np.int32)
+
+            cropped_frame = frame[face[0] : face[2], face[1] : face[3]]
+            mask = AI.id_face(cropped_frame)
+            with open('./resources/data/users.json', 'r') as file:
+                users_data = json.load(file)
             
-        #     if username in users_data.keys() is False:
-        #         users_data[username] = []
+            if username in users_data.keys() is False:
+                users_data[username] = []
             
-        #     users_data[username].append(str(mask))
+            users_data[username].append(str(mask))
 
-        #     with open('./resources/data/users.json', 'w') as file:
-        #         json.dump(users_data, file)
-        #     continue
+            with open('./resources/data/users.json', 'w') as file:
+                json.dump(users_data, file)
+            continue
 
         if(option == "all" or option == "face"):
             faces = AI.find_faces(frame.copy())
@@ -142,7 +134,9 @@ def main():
                     x1, x2, y1, y2, args = slice_info
                     _, xa, _ = args.split(',')[0].split(':')
                     _, ya, _ = args.split(',')[1].split(':')
+                    print(frame.shape)
                     aux = frame.copy()[y1 : y2, x1 : x2]
+                    print(aux.shape)
                     slice_faces = AI.find_faces(aux)
                     if slice_faces is not None:
                         for f in slice_faces:
@@ -153,7 +147,7 @@ def main():
                             f_aux[0][3] += float(ya)
                             faces.append(f_aux)
 
-            if faces is not None or faces is not []:
+            if faces is not [] or faces != []:
                 print(len(faces))
                 to_be_sent["faces"]  = []
                 to_be_sent["scores"] = []
@@ -198,7 +192,7 @@ def main():
                 print("None")
 
         to_be_sent["ID"] = ID
-        to_be_sent["offset"] = "0,0"
+        to_be_sent["offset"] = width + "," + height
         to_be_sent["sent-time"] = "t" + str(time.time())
         FDBK.send(json.dumps(to_be_sent))
 
